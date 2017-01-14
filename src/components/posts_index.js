3d5682e7077015e4f2ce2d198e2fe02e81f60775
mlombard82 <mlombard82@gmail.com>
2017-01-13 21:18:54 -0500

    implement the link to our individual post_show view(Dynamic route params)

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -12,11 +12,13 @@ renderPosts(){
   return this.props.posts.map((post) => {
     return (
       <li className="list-group-item" key={post.id} >
+        <Link to={"posts/" + post.id} >
         <span className="pull-xs-right">{post.categories}</span>
-        <strong>{post.title}</strong>  
+        <strong>{post.title}</strong> 
+        </Link> 
       </li>
     );
-  });
+  }); 
 }
 
   render() {
